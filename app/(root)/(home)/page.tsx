07d9f9d8fe9bc851ee341b3f3776e300f6733a68
Adimachukwu Benjamin <thegreatben27@gmail.com>
2024-05-01 18:24:18 +0100

    Add assets and layouts

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 const Home = () => {
   return (
     <div>
-      <h1 className="h1-bold">this is Benjamin</h1>
+     
       <UserButton afterSignOutUrl="/" />
     </div>
   );
